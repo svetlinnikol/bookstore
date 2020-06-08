411a329e62b3229d9fa6d0cfee58d7cb4156d872
s.nikolov <svetlin.nikolov.91@gmail.com>
2020-06-08 16:25:21 +0300

    make user-service spring boot application

---
+++ user-service/src/main/java/com/opentag/bookstore/userservice/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.opentag.bookstore.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for the Spring boot application.
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
